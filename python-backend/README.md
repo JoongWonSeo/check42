545a3a5878a5d33a72c1bea28ae0da9b35d0e5a5
Joong-Won Seo <Joong-Won.Seo@tum.de>
2022-11-18 02:54:32 +0100

    example search API with automatic query build and optional args

--- python-backend/README.md
+++ python-backend/README.md
@@ -15,3 +15,26 @@ For offers.csv (*WARNING: WILL TAKE A LONG TIME*):
 ```
 python create_db.py offers.csv offers 1000000
 ```
+
+
+## Start the REST API Server
+
+Debug (*WARNING: DEBUG MODE HAS ARBITRARY CODE EXECUTION*):
+```
+flask --app server --debug run
+```
+
+Non-debug:
+```
+flask --app server run
+```
+
+
+## Some observations about the given dataset
+
+### offers.csv
+- It's huge: 72353411 rows and >14GB!
+- On my laptop, a query that requires a search through the entire DB with python and sqlite3 takes around 30s
+- Since the dataset is only Mallorca, inbounddepartureairport = outboundarrivalairport = PMI for all rows
+- inboundarrivalairport = outbounddepartureairport for all rows, which makes sense I guess
+- should definitely make an index based on e.g. price so that you can sort by price and also for date for range search!
--- python-backend/server.py
+++ python-backend/server.py
@@ -4,7 +4,8 @@ from flask import Flask, jsonify, request
 
 
 # connect to database
-con = sqlite3.connect('file:database.db?mode=ro', uri=True, detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
+con = sqlite3.connect('file:database.db?mode=ro', uri=True, detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES, check_same_thread=False)
+con.set_trace_callback(print)
 cur = con.cursor()
 
 # create the flask app
@@ -17,7 +18,28 @@ def index():
                     'email': 'alice@outlook.com'})
 
 @app.route('/search')
-def index():
-    price = request.args.get('price')
-    return jsonify({'name': 'alice',
-                    'email': 'alice@outlook.com'})
+def search():
+    # get url parameters
+    g = request.args.get
+    
+    # create query constraints based on given parameters
+    conds_args = list(filter(lambda pair: pair[1] is not None, [
+        ('o.price >= ?', g('price_min')),
+        ('o.price <= ?', g('price_max')),
+        ('o.outbounddepartureairport = ?', g('airport')),
+    ]))
+
+    # build query
+    query = f'''
+SELECT DISTINCT h.name, o.price, o.departuredate, o.returndate, o.roomtype
+FROM hotels as h INNER JOIN offers as o ON h.id = o.hotelid
+WHERE {' AND '.join([c for c, _ in conds_args]) if len(conds_args)>0 else 'TRUE'}
+LIMIT 100
+'''
+
+    # execute query
+    results = cur.execute(query, [a for _, a in conds_args]).fetchall()
+    results = [{
+        'hotel': hotel, 'price': price, 'dep': dep, 'ret': ret, 'room': room
+        } for hotel, price, dep, ret, room in results]
+    return jsonify(results)
